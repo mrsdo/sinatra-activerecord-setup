07e8d37a46dbd82fe5e6048a750be115b3c9f6b9
mrsdo <me@martidolce.com>
2021-01-03 23:24:14 -0500

    Added Rakefile and rake db:migrate SINATRA_ENV=development

--- db/migrate/20210104040624_create_dogs.rb
+++ db/migrate/20210104040624_create_dogs.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class CreateDogs < ActiveRecord::Migration[5.2]
-
-    def change
-      create_table :dogs do |t|
-        t.string :name
-        t.string :breed
-      end
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
     end
-
+  end
 end
