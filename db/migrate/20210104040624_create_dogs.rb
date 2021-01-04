a8696e01a9e943cf80eafe5a2a58d470c14c3696
mrsdo <me@martidolce.com>
2021-01-03 23:22:24 -0500

    Done.

--- db/migrate/20210104040624_create_dogs.rb
+++ db/migrate/20210104040624_create_dogs.rb
@@ -9,15 +9,4 @@ class CreateDogs < ActiveRecord::Migration[5.2]
       end
     end
 
-
-  # def up
-  #   create_table :dogs do |t|
-  #     t.string :name
-  #     t.string :breed
-  #   end
-  # end
-  #
-  # def down
-  #   drop_table :dogs
-  # end
 end
